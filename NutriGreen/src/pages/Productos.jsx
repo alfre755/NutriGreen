1cfd296b9e9744df4196b4e238761b65fa8cb36a
alfre755 <4lfr3755@gmail.com>
2024-09-09 16:07:51 -0300

    feat page
    Se agrega pagina productos

---
+++ NutriGreen/src/pages/Productos.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Productos(props) {
+  return (
+    <div>Productos</div>
+  )
+}
+
+Productos.propTypes = {}
+
+export default Productos
