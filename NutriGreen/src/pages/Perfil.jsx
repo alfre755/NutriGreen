cedf8565d83bd347250929376ea67dbc94132963
alfre755 <4lfr3755@gmail.com>
2024-09-10 13:52:25 -0300

    feat pages
    Se agrega pagina que se encargara del perfil del cliente

---
+++ NutriGreen/src/pages/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Perfil(props) {
+  return (
+    <div>Perfil</div>
+  )
+}
+
+Perfil.propTypes = {}
+
+export default Perfil
