87cec80c69027f1dc0f6eca615b36cff348b811f
alfre755 <4lfr3755@gmail.com>
2024-11-18 14:26:57 -0300

    Feat component
    Se desarrolla el componente que se utilizara para mostrar los planes existentes

--- NutriGreen/src/pages/Planes.jsx
+++ NutriGreen/src/pages/Planes.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ListarCategorias from "../components/LayaoutTemplate/ListarCategorias";
 
 function Planes(props) {
-  return <div>Planes</div>;
+  const objPlanes = [
+    { id: "1", title: "Plan1", imagen: "public/evento5_4.jpg" },
+    { id: "2", title: "Plan2", imagen: "public/evento5_4.jpg" },
+    { id: "3", title: "Plan3", imagen: "public/evento5_4.jpg" },
+  ];
+  return (
+    <div>
+      <ListarCategorias
+        {...{ title: "Planes disponibles", categorias: objPlanes }}
+      />
+    </div>
+  );
 }
 
 Planes.propTypes = {};
