cc088c867a084df43f0b7fcee0f478793f701ce7
alfre755 <4lfr3755@gmail.com>
2024-12-05 07:06:17 -0300

    Feat: New context
    Se agrega contexto nuevo y componente necesario para manejar y renderizar popUp

--- NutriGreen/src/core/App.jsx
+++ NutriGreen/src/core/App.jsx
@@ -5,15 +5,20 @@ import SimpleRouterApp from "./SimpleRouterApp";
 import MainView from "../components/LayaoutTemplate/MainView";
 import "./App.css";
 import DataProvider from "../hooks/useData";
+import Popup from "../components/Main/PopUp";
+import { PopupProvider } from "../hooks/UsePopUp";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <DataProvider>
+          <PopupProvider>
           <MainView>
             <SimpleRouterApp />
           </MainView>
+          <Popup />
+          </PopupProvider>
         </DataProvider>
       </ThemeProvider>
     </>
