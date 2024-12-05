037f2db20b975641750f860d206b5c41eb0e3cea
alfre755 <4lfr3755@gmail.com>
2024-12-05 07:07:58 -0300

    Fix: Estructura
    Se modifica la estructura de la pagina para dividirla en diferentes secciones y que sea mas legible manteniendo la funcionalidad

--- NutriGreen/src/pages/Administracion.jsx
+++ NutriGreen/src/pages/Administracion.jsx
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-import { useNavigate } from "react-router-dom";
-import CRUDSection from "../sections/Custom/CRUDSection";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
-import UsuarioAgregarForm from "../components/Custom/UsuarioAgregarForm"; // Formulario para crear usuarios
-import UsuarioModificarForm from "../components/Custom/UsuarioModificarForm"; // Formulario para modificar usuarios
-import ListarUsuarios from "../components/Custom/ListarUsuarios";
-import Eliminar from "../sections/Custom/Eliminar";
+import UsuariosCRUDSection from "../sections/Custom/UsuariosCRUDSection";
+import ProductosCRUDSection from "../sections/Custom/ProductosCRUDSection";
 
 const AdministracionStyle = styled.section`
   display: flex;
@@ -26,54 +20,6 @@ const AdministracionStyle = styled.section`
 `;
 
 function Administracion(props) {
-  const navigate = useNavigate();
-  const [isPopupOpen, setPopupOpen] = useState(false);
-  const [activeAction, setActiveAction] = useState(""); // Para rastrear la acción activa (crear, modificar, eliminar, ver)
-  const [activeCRUD, setActiveCRUD] = useState(""); // Para rastrear el tipo de CRUD (usuarios, productos, planes)
-
-  const handleOpenPopup = (action, crud) => {
-    setActiveAction(action);
-    setActiveCRUD(crud);
-    setPopupOpen(true);
-    return true;
-  };
-
-  const handleClosePopup = () => {
-    setPopupOpen(false);
-    setActiveAction("");
-    setActiveCRUD("");
-  };
-
-  const renderPopupContent = () => {
-    if (activeCRUD === "Usuarios") {
-      switch (activeAction) {
-        case "Crear":
-          return <UsuarioAgregarForm />;
-        case "Modificar":
-          return (
-            <ListarUsuarios
-              {...{ accion: "Editar", ruta: "ModificarUsuario" }}
-            />
-          );
-        case "Eliminar":
-          return (
-            <ListarUsuarios
-              {...{ accion: "Eliminar", ruta: "EliminarUsuario" }}
-            />
-          );
-        case "Ver":
-          return (
-            <ListarUsuarios
-              {...{ accion: "Ver", ruta: "ModificarUsuario" }}
-            />
-          );
-        default:
-          return null;
-      }
-    }
-    return <p>Formulario no disponible para esta acción</p>;
-  };
-
   return (
     <AdministracionStyle>
       <div className="headerWrapper">
@@ -81,105 +27,12 @@ function Administracion(props) {
       </div>
       <div className="CRUDWrapper">
         {/* CRUD Usuarios */}
-        <CRUDSection
-          {...{
-            title: "CRUD Usuarios",
-            buttonConfig: [
-              {
-                label: "Crear Usuarios",
-                type: 1,
-                onClick: () => handleOpenPopup("Crear", "Usuarios"),
-              },
-              {
-                label: "Modificar Usuarios",
-                type: 1,
-                onClick: () => handleOpenPopup("Modificar", "Usuarios"),
-              },
-              {
-                label: "Eliminar Usuarios",
-                type: 1,
-                onClick: () => handleOpenPopup("Eliminar", "Usuarios"),
-              },
-              {
-                label: "Ver Usuarios",
-                type: 1,
-                onClick: () => handleOpenPopup("Ver", "Usuarios"),
-              },
-            ],
-          }}
-        />
+        <UsuariosCRUDSection />
         {/* CRUD Productos */}
-        <CRUDSection
-          {...{
-            title: "CRUD Productos",
-            buttonConfig: [
-              {
-                label: "Crear Productos",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Modificar Productos",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Eliminar Productos",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Ver Productos",
-                type: 1,
-                onClick: () => {},
-              },
-            ],
-          }}
-        />
-        {/* CRUD Planes */}
-        <CRUDSection
-          {...{
-            title: "CRUD Planes",
-            buttonConfig: [
-              {
-                label: "Crear Planes",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Modificar Planes",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Eliminar Planes",
-                type: 1,
-                onClick: () => {},
-              },
-              {
-                label: "Ver Planes",
-                type: 1,
-                onClick: () => {},
-              },
-            ],
-          }}
-        />
-      </div>
+        <ProductosCRUDSection />
 
-      {/* Popup Dinámico */}
-      <Popup
-        open={isPopupOpen}
-        onClose={handleClosePopup}
-        position="right center"
-      >
-        <div>
-          <h3>
-            {activeAction} {activeCRUD}
-          </h3>
-          {renderPopupContent()}
-          <button onClick={handleClosePopup}>Cerrar</button>
-        </div>
-      </Popup>
+        {/* CRUD Planes */}
+      </div>
     </AdministracionStyle>
   );
 }
