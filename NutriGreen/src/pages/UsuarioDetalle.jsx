07397b57f0c50bae9ab96f698d433f97a9bd89a9
alfre755 <4lfr3755@gmail.com>
2024-12-05 07:08:26 -0300

    Feat: New Page
    Se agrega nueva pagina para mostrar los detalles de los usuarios

---
+++ NutriGreen/src/pages/UsuarioDetalle.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function UsuarioDetalle(props) {
+  return <div>UsuarioDetalle</div>;
+}
+
+UsuarioDetalle.propTypes = {};
+
+export default UsuarioDetalle;
